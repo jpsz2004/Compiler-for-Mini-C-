7edfce533312255055dd59e8fa20188cf86516bd
Juan Pablo Sánchez Zapata <106684652+jpsz2004@users.noreply.github.com>
2024-11-05 00:32:52 -0500

    Update CppChecker.py

--- Analizadores/CppChecker.py
+++ Analizadores/CppChecker.py
@@ -1,6 +1,3 @@
-from CppAST import *
-from stdlib import *
-
 '''
 ****************************************************************************************
 ******************************** ANALIZADOR SEMÁNTICO **********************************
@@ -15,20 +12,24 @@ los tipos de datos sean correctos, que las funciones sean
 declaradas antes de ser usadas, etc.
 
 Para ello, se recorre el AST generado por el parser y se
-verifican las reglas semánticas del lenguaje.
+verifican las reglas semánticas del lenguaje.   
 
 ****************************************************************************************
 
 '''
 
+from CppAST import *
+from stdlib import * #Con algunas de los nombres de funciones reservadas de C++
+
+
 class SymbolTable:
     '''
     TABLA DE SÍMBOLOS
 
-    La tabla de símbolos es un objeto que mantiene un diccionario
+    La tabla de símbolos es un objet que mantiene un diccionario
     de nombres de símbolos y nodos asociados a esos símbolos.
 
-    Existe una tabla de símbolos separada para cada elemento de código
+    Existe una tabla de símbolos separad para cada elemento de código
     que lo requiera, como funciones, clases, bloques, etc.
     '''
 
@@ -278,5 +279,219 @@ class Checker(Visitor):
         # 2. Se visita el cuerpo del while
         self.visit(node.body_stmt, env)
         InLoop=False
+    
+    def visit(self, node: ForStmt, env: SymbolTable):
+        '''
+        1. Visitar la inicialización
+        2. Visitar la condición
+        3. Visitar la actualización
+        4. Visitar el cuerpo del for
+        '''
 
+        global InLoop
+        env = SymbolTable(env)
+
+        # 1. Se visita la inicialización del for
+        self.visit(node.init, env)
+        # 2. Se visita la condición del for
+        self.visit(node.cond, env)
+        # 3. Se visita la actualización del for
+        self.visit(node.update, env)
+
+        InLoop=True
+        # 4. Se visita el cuerpo del for
+        self.visit(node.body_stmt, env)
+        InLoop=False
+    
+    def visit(self, node: ReturnStmt, env: SymbolTable):
+        '''
+        Visitar la expresión de retorno
+        '''
+
+        if node.expr:
+            self.visit(node.expr, env)
+
+    def visit(self, node: ExprStmt, env: SymbolTable):
+        '''
+        Visitar la expresión
+        '''
+
+        if node.expr is not None:
+            self.visit(node.expr, env)
+    
+    def visit(self, node: BreakStmt, env: SymbolTable):
+        '''
+        Verificar que el break esté dentro de un ciclo
+        '''
+
+        if not InLoop:
+            self.error(node, f"Error: '{node.name}' fuera de un ciclo")
+    
+    def visit(self, node: ContinueStmt, env: SymbolTable):
+        '''
+        Verificar que el continue esté dentro de un ciclo
+        '''
+
+        if not InLoop:
+            self.error(node, f"Error: '{node.name}' fuera de un ciclo")
+    
+    def visit(self, node: SizeStmt, env: SymbolTable):
+        '''
+        Visitar la expresión
+        '''
+        if node.expr:
+            self.visit(node.expr, env)
+    
+    def visit(self, node: CompoundStmt, env: SymbolTable):
+        '''
+        Visitar las instrucciones
+        '''
+        env = SymbolTable(env)
+
+        for statement in node.stmts:
+            self.visit(statement, env)
+    
+    def visit(self, node: NullStmt, env: SymbolTable):
+        '''
+        No hacer nada
+        '''
+        pass
+
+    #Visita de nodos de expresiones
+    
+    def visit(self, node: LiteralExpr, env: SymbolTable):
+        '''
+        No hacer nada
+        '''
+        pass
+
+    def visit(self, node: CallExpr, env: SymbolTable):
+        '''
+        1. Buscar la función exista en la tabla de símbolos
+        2. Validar el número de argumentos pasados
+        3. Validar el tipo de los argumentos
+        4. Visitar las expr de los argumentos
+        '''
+
+        # Buscar la función en la tabla de símbolos
+        self.visit(node.func, env)
+        result = env.getSymbol(node.func.name)
+
+        # Visita las expresiones de los argumentos
+        if node.args is not None:
+            for argument in node.args:
+                self.visit(argument, env)
         
+        # Verifica que sea una declaración de una función y la cantidad de argumentos
+        if result is FuncDeclStmt:
+            if result is not None:
+                if result.params is not None:
+                    if len(result.params) != len(node.args):
+                        self.error(node, f"Error de checker. La funcion '{node.func.name}' esperaba {len(result.params)} argumentos, pero se pasaron {len(node.args)}")
+        
+        # Verifica el tipo de los argumentos
+        for argument, param in zip(node.args, result.params):
+            arg_type = self.visit(argument, env)
+
+            # Verifica que el tipo del argumento sea el mismo que el tipo del parámetro
+            if arg_type != param.return_type:
+                self.error(
+                node,
+                f"Tipo de argumento inválido para '{param.name}' en la llamada a '{node.func.name}': "
+                f"esperado '{param.return_type}', pero se encontró '{arg_type}'."
+            )
+        
+        return result.return_type
+    
+    def visit(self, node: VarExpr, env: SymbolTable):
+        '''
+        Buscar la variable en la tabla de símbolos para validar 
+        que todo Identificador debe ser declarado previamente
+        '''
+
+        result = env.getSymbol(node.name)
+        if result is None:
+            self.error(node, f"Error de checker. La variable '{node.name}' no ha sido declarada")
+        
+        return result.return_type
+    
+    def visit(self, node: UnaryOpExpr, env: SymbolTable):
+        '''
+        Visitar la expresión
+        '''
+
+        self.visit(node.expr, env)
+    
+    def visit(self, node: BinaryOpExpr, env: SymbolTable):
+        '''
+        Visitar el hijo izquierdo y el hijo derecho
+        '''
+
+        self.visit(node.left, env)
+        self.visit(node.right, env)
+    
+    def visit(self, node: LogicalExpr, env: SymbolTable):
+        '''
+        Visitar el hijo izquierdo y el hijo derecho
+        '''
+
+        self.visit(node.left, env)
+        self.visit(node.right, env)
+    
+    def visit(self, node: AssignExpr, env: SymbolTable):
+        '''
+        1. Verificar que la variable exista en la tabla de símbolos 
+        2. Visitar la expresión
+        3. Verificar que el tipo de la expresión sea el mismo que el tipo de la variable
+        '''
+
+        # 1. Verificar que la variable exista en la tabla de símbolos
+        result = env.getSymbol(node.name)
+        if result is None:
+            self.error(node, f"Error de checker. La variable '{node.name}' no ha sido declarada")
+        
+        # 2. Visitar la expresión
+        self.visit(node.expr, env)
+
+        # 3. Verificar que el tipo de la expresión sea el mismo que el tipo de la variable
+        if result.return_type != node.expr.return_type:
+            self.error(node, f"Error de checker. Se esperaba una expresión de tipo '{result.return_type}' pero se encontró una expresión de tipo '{node.expr.return_type}'")
+        
+        return result.return_type
+    
+    def visit(self, node: Set, env: SymbolTable):
+        '''
+        1. Buscar objeto en la tabla de símbolos
+        2. Buscar nombre en la tabla de símbolos
+        3. Visitar la expresión
+        
+        '''
+
+        self.visit(node.obj, env)
+        name = env.getSymbol(node.name)
+
+        if name is None:
+            self.error(node, f"Error de checker. El nombre (set) '{node.name}' no ha sido declarado")
+        
+        self.visit(node.expr, env)
+
+    def visit(self, node: Get, env: SymbolTable):
+        '''
+        1. Buscar objeto en la tabla de símbolos
+        2. Buscar nombre en la tabla de símbolos
+        '''
+
+        self.visit(node.obj, env)
+        name = env.getSymbol(node.name)
+
+        if name is None:
+            self.error(node, f"Error de checker. El nombre (Get) '{node.name}' no ha sido declarado")
+        
+        return name.return_type
+    
+    def visit(self, node: ThisExpr, env: SymbolTable):
+        '''
+        No hacer nada
+        '''
+        pass
+    
